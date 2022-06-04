9254663e9f3dc75439a311ea8c2c8b1d2a5756bd
unknown <adham.ghazali@gmail.com>
2022-06-05 01:44:25 +0300

    'add-rnn'

--- stgen/stgen.py
+++ stgen/stgen.py
@@ -5,16 +5,26 @@ from transformers import T5EncoderModel
 
 class stgen(torch.nn.Module):
 
-    def __init__(self,model_name):
+    def __init__(self,model_name,embeddings_dims,hidden_size):
         super().__init__()
 
-        self.t5 = T5EncoderModel.from_pretrained(model_name)
+        self.hidden_size = hidden_size
+        self.embeddings_dims = embeddings_dims
 
+        all_letters = string.ascii_letters + " .,;'-"
+        self.output_size = len(all_letters) + 1  # Plus EOS marker
+
+        self.t5 = T5EncoderModel.from_pretrained(model_name)
         for param in self.t5.parameters():
             param.requires_grad = False
 
-        self.fc1 = nn.Linear(512, 1024) # 512 if using t5-small
-        #self.some_layer_tbd
+
+        self.i2h = nn.Linear(embeddings_dims + hidden_size, hidden_size)
+        self.i2o = nn.Linear(embeddings_dims + hidden_size, self.output_size)
+        self.o2o = nn.Linear(hidden_size + self.output_size, self.output_size)
+        self.dropout = nn.Dropout(0.1)
+        self.softmax = nn.LogSoftmax(dim=1)
+
 
 
     def get_text_emb(self, tokens, mask):
@@ -24,9 +34,24 @@ class stgen(torch.nn.Module):
 
         return outputs
 
-    def forward(self, tokens,mask):
 
+
+    def forward(self, tokens,mask, hidden):
         text_outputs = self.get_text_emb(tokens, mask)
-        x=self.some_layer_tbd(text_outputs)
 
-        return x
+        input_combined = torch.cat(( text_outputs, hidden), 1)
+        hidden = self.i2h(input_combined)
+        output = self.i2o(input_combined)
+        output_combined = torch.cat((hidden, output), 1)
+        output = self.o2o(output_combined)
+        output = self.dropout(output)
+        output = self.softmax(output)
+
+
+        return output, hidden
+
+        def initHidden(self):
+            return torch.zeros(1, self.hidden_size)
+
+
+
