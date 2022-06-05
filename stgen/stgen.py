8cf2eefa4f93f9a32a2fd266747cfa48cb9aeae4
unknown <adham.ghazali@gmail.com>
2022-06-05 15:05:27 +0300

    'fixes

--- stgen/stgen.py
+++ stgen/stgen.py
@@ -2,6 +2,13 @@ import torch as th
 import torch.nn as nn
 from transformers import T5EncoderModel
 from stgen import configs
+from stgen.configs import T5_CONFIGS
+import string
+from transformers import T5Tokenizer, T5EncoderModel, AutoTokenizer, AutoModelForSeq2SeqLM
+import torch.nn.functional as f
+
+
+
 MAX_LENGTH=128
 
 class tokenizer:
@@ -24,10 +31,11 @@ class tokenizer:
 
         self.tokenizer = tokenizer_class.from_pretrained(model_name)
 
-        if torch.cuda.is_available():
-            self.t5_model = self.t5_model.cuda()
-        self.device = next(self.t5_model.parameters()).device
-        print("the device is", self.device)
+        #if th.cuda.is_available():
+        #   self.t5_model = self.t5_model.cuda()
+        #self.device = next(self.t5_model.parameters()).device
+        #print("the device is", self.device)
+        self.device= th.device('cuda')
 
     def get_tokens_masks(self,texts):
 
@@ -44,23 +52,30 @@ class tokenizer:
 
 class stgen(th.nn.Module):
 
-    def __init__(self,model_name,hidden_size):
+    def __init__(self,model_name,hidden_size,batch_size):
         super().__init__()
 
         self.hidden_size = hidden_size
-        self.embeddings_dims = configs.embeddings_dims
+        self.batch_size=batch_size
+        self.embeddings_dims = configs.embedding_dims
 
         all_letters = string.ascii_letters + " .,;'-"
         self.output_size = len(all_letters) + 1  # Plus EOS marker
 
         self.t5 = T5EncoderModel.from_pretrained(model_name)
+
         for param in self.t5.parameters():
             param.requires_grad = False
-        self.fc1=nn.linear(configs.embeddings_dims,configs.embeddings_dims)
+
+        if th.cuda.is_available():
+            self.t5 = self.t5.cuda()
 
 
-        self.i2h = nn.Linear(configs.embeddings_dims + hidden_size, hidden_size)
-        self.i2o = nn.Linear(configs.embeddings_dims + hidden_size, self.output_size)
+        self.fc1=nn.Linear(configs.embedding_dims,configs.embedding_dims)
+
+
+        self.i2h = nn.Linear(configs.embedding_dims + hidden_size, hidden_size)
+        self.i2o = nn.Linear(configs.embedding_dims + hidden_size, self.output_size)
         self.o2o = nn.Linear(hidden_size + self.output_size, self.output_size)
         self.dropout = nn.Dropout(0.1)
         self.softmax = nn.LogSoftmax(dim=1)
@@ -70,16 +85,21 @@ class stgen(th.nn.Module):
     def get_text_emb(self, tokens, mask):
         with th.no_grad():
             encoded_text = self.t5(input_ids=tokens, attention_mask=mask)['last_hidden_state'].float()
-            outputs = self.fc1(encoded_text[:, -1])
+            print('encoded_text shape', encoded_text.shape)
+            embeddings = self.fc1(encoded_text[:, -1])
+            print('embeddings_shape before   ',embeddings.shape)
 
-        return outputs
 
-    def rnn(self,input_texts,hidden):
 
-        input_combined = torch.cat((input_texts, hidden), 1)
+        return embeddings
+
+    def rnn(self,embeddings,hidden):
+        print('concat shape', embeddings.shape, hidden.shape)
+        input_combined = th.cat((embeddings, hidden), 1)
+
         hidden = self.i2h(input_combined)
         output = self.i2o(input_combined)
-        output_combined = torch.cat((hidden, output), 1)
+        output_combined = th.cat((hidden, output), 1)
         output = self.o2o(output_combined)
         output = self.dropout(output)
         output = self.softmax(output)
@@ -89,16 +109,18 @@ class stgen(th.nn.Module):
 
 
     def forward(self, tokens,mask, hidden):
-        text_outputs = self.get_text_emb(tokens, mask) # Transformer
 
-        output, hidden=rrn(text_outputs,hidden) #Character RNN
+        embeddings = self.get_text_emb(tokens, mask) # Transformer
+        print('before shape', embeddings.shape, hidden.shape)
 
-        return output
+        output, hidden=self.rnn(embeddings,hidden) #Character RNN
+
+        return output, hidden
 
 
 
     def initHidden(self):
-        return torch.zeros(1, self.hidden_size)
+        return th.zeros(self.batch_size, self.hidden_size)
 
 
 
